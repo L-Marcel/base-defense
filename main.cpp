7e3292530c16645da3f1fd49737f5deb1b93d8ab
Pedro Paulo <pedropaulo062011@gmail.com>
2024-07-15 01:12:03 -0300

    Fazendo o player iniciar no 'meio' da tela

--- main.cpp
+++ main.cpp
@@ -9,6 +9,8 @@ int main() {
     Player player("assets/player.png", Box(12, 14, 24, 28));
     player.scale(4);
     player.animate(8, 1, false);
+    player.x = 400;
+    player.y = 200;
 
     gp.addObject(&player);
     gp.execute();
