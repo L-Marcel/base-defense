b188e510f7f910c5b24bff59f1faa35969e4853f
Pedro Paulo <pedropaulo062011@gmail.com>
2024-08-07 23:10:39 -0300

    Minha defesa!

--- include/Objects/image.hpp
+++ include/Objects/image.hpp
@@ -1,6 +1,9 @@
 #pragma once
 #include <Objects/object_2d.hpp>
 
+//MOTIVAÇÃO PARA CRIAÇÃO DE UMA CLASSE TÃO "PRIMOROSA" QUE NEM A QUE ESTÁ DIANTE DE SEUS OLHOS:
+//Estava tentando criar uma variável do tipo Object2D, mas tava bugando a classe player, aí criei essa classe para servir como imagem ou animação de algo que não tem nenhuma peculiaridade.
+
 namespace Game{
   class Image : public Object2D{
     public:
